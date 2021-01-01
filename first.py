e6fd0b04960b0377c83921ec33d9dd211aed0bb5
837477 <8374770@gmail.com>
2021-01-01 22:39:21 +0900

    First Threading Study

---
+++ first.py
@@ -0,0 +1,13 @@
+import threading
+import time
+
+def threadworker(i):
+    print(str(i) + " Thread has entered the 'Running' State")
+    time.sleep(3)
+    print(str(i) + " Thread is terminating")
+
+if __name__ == "__main__":
+    for i in range(1, 4):
+        T = threading.Thread(target=threadworker, args=(i,))
+        T.start()
+    
